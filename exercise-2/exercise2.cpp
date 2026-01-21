f7b6b69e399509f38237ecdcf49610fc821bfdb4
nclvrl <nicolecdverial@gmail.com>
2026-01-21 22:24:22 +0800

    Fixed some comments

--- exercise-2/exercise2.cpp
+++ exercise-2/exercise2.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 // This function goes through a string, per character, and checks if it is a digit.
-// If it encounters at least one that isn't a digit, it will return false
-// If it doesn't enounter a non-digit, it will return true
+// If it encounters at least one that isn't a digit, it will return false.
+// Else if it doesn't enounter a non-digit, it will return true.
 bool isNumber(string s) {
     for (char c : s) if (!isdigit(c)) return false;
     return true;
@@ -11,18 +11,18 @@ bool isNumber(string s) {
 
 int main() {
     int N; // Initialize N
-    cin >> N; // read the first piece of data, load into N
+    cin >> N; // Read the first piece of data, load it into N
     for (int i=0; i<N; i++) { // Iterate through N lines
-        int x; int y; string data; // initialize variables
-        cin >> x >> y; // read x and y using cin
-        cin.ignore(); // throw away the extra space between y and data 
-        getline(cin, data); // read the entire line and pass it into data 
-        cout << "Agent #" << i+1 << " is at ("  << x << ", " << y << ")" << endl; // format output to show position of agent
-        if (isNumber(data)) { // if the data is a number, not a message
-            cout << "Agent #" << i+1 << " holds up the number: " << data << endl; // format output to say that the agent holds up a number
-            continue; // skip and go to next agent
+        int x; int y; string data; // Initialize variables
+        cin >> x >> y; // Read x and y using cin
+        cin.ignore(); // Throw away the extra space between y and data 
+        getline(cin, data); // Read the entire line and pass it into data 
+        cout << "Agent #" << i+1 << " is at ("  << x << ", " << y << ")" << endl; // Format output to show position of agent
+        if (isNumber(data)) { // If the data is a number, an agent holds a number up
+            cout << "Agent #" << i+1 << " holds up the number: " << data << endl; // Format output to show number held up by agent
+            continue; // Skip and go to next agent
         }
-        cout << "Agent #" << i+1 << " yells: \"" << data << "\"" << endl; // if data is not a number, format output to say that the agent yells out a message 
+        cout << "Agent #" << i+1 << " yells: \"" << data << "\"" << endl; // If the data is not a number, format output to show agent's message 
     }
     return 0;
 }
